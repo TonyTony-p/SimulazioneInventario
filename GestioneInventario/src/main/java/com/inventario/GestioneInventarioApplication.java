dbf1ea3486655709f47a51e04fbd28b9326fb298
anton <anton@TwinsPc>
2025-09-27 14:07:08 +0200

    Creazione progetto springboot

---
+++ GestioneInventario/src/main/java/com/inventario/GestioneInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneInventarioApplication.class, args);
+	}
+
+}
