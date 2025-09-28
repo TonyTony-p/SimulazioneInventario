1337f0b8678e02d1c29fd68b3878b8636f842003
anton <anton@TwinsPc>
2025-09-28 18:12:14 +0200

    aggiunti metodi per la modifica, l'eliminazione di un prodotto e il listAll di tutti i prodotti paginati(testato il tutto su postman).

--- GestioneInventario/src/main/java/com/inventario/restinventario/service/ProdottoService.java
+++ GestioneInventario/src/main/java/com/inventario/restinventario/service/ProdottoService.java
@@ -1,6 +1,9 @@
 package com.inventario.restinventario.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.inventario.restinventario.dto.FornitoreDto;
 import com.inventario.restinventario.dto.ProdottoCreateResponseDto;
@@ -10,6 +13,9 @@ import com.inventario.restinventario.model.Fornitore;
 import com.inventario.restinventario.model.Prodotto;
 import com.inventario.restinventario.repository.FornitoreRepository;
 import com.inventario.restinventario.repository.ProdottoRepository;
+
+import it.studentiscuolajpa.reststudentiscuola.dto.PageResponse;
+import jakarta.validation.Valid;
 
 @Service
 
@@ -35,9 +41,16 @@ public class ProdottoService {
 	    return dto;
 	}
 	
+	@Transactional(readOnly = true)
+	
+	public PageResponse<ProdottoCreateResponseDto> listAll(Pageable pageable){
+		Page<Prodotto> page = prodottoRepo.findAll(pageable); 
+		Page<ProdottoCreateResponseDto> mapper = page.map(p -> new ProdottoCreateResponseDto(p.getId(),p.getNome(),p.getQuantita(),p.getDescrizione()));
+		return PageResponse.from(mapper);
+	}	
 	
 
-
+	@Transactional
 	public ProdottoCreateResponseDto create(ProdottoFormDto dto) {
 	    if (dto.getId() != null) {
 	        throw new IllegalArgumentException("id non valido per la creazione");
@@ -64,9 +77,38 @@ public class ProdottoService {
 	        prodotto.getDescrizione()
 	   );
 	}
-
 	
+	@Transactional
+	public Prodotto update(@Valid ProdottoFormDto input) {
+	    Long id = input.getId();
+	    if (id == null || id <= 0) {
+	        throw new IllegalArgumentException("ID non valido per update");
+	    }
 
+	    Prodotto p = prodottoRepo.findById(id)
+	            .orElseThrow(() -> new IllegalArgumentException("Prodotto non trovato: id=" + id));
+
+	    p.setNome(input.getNome());
+	    p.setQuantita(input.getQuantita());
+	    p.setDescrizione(input.getDescrizione());
+
+	    
+	    if (input.getFornitoreId() != null) {
+	        Fornitore fornitore = fornitoreRepo.findById(input.getFornitoreId())
+	                .orElseThrow(() -> new IllegalArgumentException("Fornitore non trovato con id=" + input.getFornitoreId()));
+	        p.setFornitore(fornitore);
+	    }
+
+	    return p;
+	}
 	
+	@Transactional
+	public void deleteById(Long id) {
+		if(!prodottoRepo.existsById(id)) {
+			throw new IllegalArgumentException("Prodotto non trovato con ID: " + id);
+		}
+		
+		prodottoRepo.deleteById(id);
+	}
 
 }
--- GestioneInventario/src/main/java/com/inventario/restinventario/web/ProdottoController.java
+++ GestioneInventario/src/main/java/com/inventario/restinventario/web/ProdottoController.java
@@ -1,15 +1,24 @@
 package com.inventario.restinventario.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inventario.restinventario.dto.ProdottoCreateResponseDto;
+import com.inventario.restinventario.dto.ProdottoDto;
 import com.inventario.restinventario.dto.ProdottoFormDto;
 import com.inventario.restinventario.service.ProdottoService;
+
+import it.studentiscuolajpa.reststudentiscuola.dto.PageResponse;
 
 @RestController
 @RequestMapping("api/prodotti")
@@ -20,11 +29,28 @@ public class ProdottoController {
 	
 	private ProdottoService prodottoService;
 	
+	@GetMapping
+	public PageResponse<ProdottoCreateResponseDto> allProdotti(@PageableDefault(size = 10) Pageable pageable){
+		return prodottoService.listAll(pageable);
+		}
+	
 	@PostMapping
 	public ResponseEntity<ProdottoCreateResponseDto> crea(@RequestBody ProdottoFormDto prodottoDto){
 		var created = prodottoService.create(prodottoDto);
 		return ResponseEntity.status(201).body(created);
 	} 
+	
+	@PutMapping
+	public ResponseEntity<ProdottoDto>update(@RequestBody ProdottoFormDto prodottoDto){
+		prodottoService.update(prodottoDto);
+		return ResponseEntity.ok().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> eliminaProdottobyId(@PathVariable Long id){
+		prodottoService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 
 }
